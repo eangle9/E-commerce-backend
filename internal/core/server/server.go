74445992ee24d217f436d863b47719620a0fcd68
eangle9 <engdaworkyismaw9@gmail.com>
2024-08-15 14:47:11 +0300

    insert the port in .env file

--- internal/core/server/server.go
+++ internal/core/server/server.go
@@ -4,10 +4,19 @@ import (
 	"Eccomerce-website/internal/infra/config"
 
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 )
 
 func Start(instance *gin.Engine, conf config.HttpServerConfig) error {
-	port := conf.Port
+	// viper.SetConfigFile("../.env")
+	viper.AddConfigPath("../")
+	viper.SetConfigName(".env")
+	viper.SetConfigType("env")
+	viper.ReadInConfig()
+	portKey := viper.Get("PORT")
+	port := portKey.(string)
+	// port := conf.Port
+	// port := os.Getenv("PORT")
 
 	if port == "" {
 		port = "8000"
